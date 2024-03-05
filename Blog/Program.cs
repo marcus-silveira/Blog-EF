5612ebd18d3abd9b45e194ea067b55b4ff40dc67
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-03-05 09:57:45 -0300

    criado configurações iniciais do dbSet e conexao ao banco de dados

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Blog.Data;
+using Blog.Models;
 
-Console.WriteLine("Hello, World!");
+namespace Blog;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        using var context = new BlogDataContext();
+        var category = new Category
+        {
+            Name = "ASP.NET",
+            Slug = "asp-net"
+        };
+
+        context.Categories.Add(category);
+        context.SaveChanges();
+    }
+}
