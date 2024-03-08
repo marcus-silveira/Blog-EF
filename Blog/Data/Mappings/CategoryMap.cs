144a0806e0ebc787827dfc7e3814a80063e7ade1
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-03-08 08:30:04 -0300

    Criado mapeamento fluent de category

---
+++ Blog/Data/Mappings/CategoryMap.cs
@@ -0,0 +1,37 @@
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings;
+
+public class CategoryMap : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        // Table
+        builder.ToTable("Category");
+
+        // Primary Key
+        builder.HasKey(x => x.Id);
+
+        // Identity
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn(); // PK Identity (1,1)
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasColumnName("Name")
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(80);
+
+        builder.Property(x => x.Slug)
+            .IsRequired()
+            .HasColumnName("Slug")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(80);
+
+        builder.HasIndex(x => x.Slug, "IX_Category_Slug")
+            .IsUnique();
+    }
+}
